066c2aab8f2571f542911dff653361a0f3089e93
sayed-1625 <sandraleonorhidalgoortiz@gmail.com>
2023-05-01 02:20:25 -0500

    init

---
+++ home/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse("Mi chiquita")
+from django.shortcuts import render
+
+# Create your views here.
